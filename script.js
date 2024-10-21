ae5dcf41b89c918f7e77af5601c74f86e152b53d
mahendran003-Tech <lmahinthra@gmail.com>
2024-10-21 05:22:01 -0700

    project completed

---
+++ script.js
@@ -0,0 +1,24 @@
+function sendMail(){
+    var params = {
+        name : document.getElementById("name").value ,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    };
+
+    const serviceID = "service_wyxzqwj";
+const templateID = "template_m90udh9";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("message").value = ""
+        console.log(res);
+        alert("your message sent successfully")
+    })
+    .catch((err) => console.log(err));
+}
+
+    
+    
